fab6a81be3fa0070d4b42a1d6c96c44d0de8c259
berkan bala <berkan.balaa@gmail.com>
2024-02-08 20:21:01 +0300

    08.02.2024 20-20

--- src/custom/components/imagetop/imagetop.tsx
+++ src/custom/components/imagetop/imagetop.tsx
@@ -4,7 +4,7 @@ import Image1 from "../../../common/media/images/mbappe.png";
 export default function ImageTop() {
   return (
     <div className={styles.container}>
-      <img alt="mbape" src={Image1} width={1000} height={400} />
+      <img alt="mbape" src={Image1} className={styles.images} />
     </div>
   );
 }
